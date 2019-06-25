18a2fb43b9a98484d7d46613873b9a446bb50f97
Oleksii.Zhylenko <Ppol129458>
2019-06-25 20:31:56 +0300

    StringUtils was added

--- src/main/java/com/mochallenge/chat/service/SecurityValidationService.java
+++ src/main/java/com/mochallenge/chat/service/SecurityValidationService.java
@@ -1,11 +1,16 @@
 package com.mochallenge.chat.service;
 
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.stereotype.Component;
+
+import com.mochallenge.chat.exception.ChatValidationException;
 
 @Component
 public class SecurityValidationService {
 
     void validate(String userName, String userPassword) {
-        // TODO: Throw exception if some attribute doesn't meat constraints
+        if (StringUtils.isBlank(userName) || StringUtils.isBlank(userPassword)) {
+            throw new ChatValidationException("UserName or password is blank");
+        }
     }
 }
